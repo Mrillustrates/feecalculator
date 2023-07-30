cfba89eecf0cd07cd26ee043d98792043e08ee6f
Chris <lawrenc@live.com>
2023-07-29 20:49:34 -0500

    -added in more values and Toast message to test out data

--- app/src/main/java/com/example/ebayfeecalculator/MainActivity.kt
+++ app/src/main/java/com/example/ebayfeecalculator/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.ebayfeecalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +25,29 @@ class MainActivity : AppCompatActivity() {
 
         val itemCost: EditText = findViewById(R.id.itemCost)
 
+        val sellPrice: EditText = findViewById(R.id.sellingPrice)
+
+        val buyerShipping: EditText = findViewById(R.id.shippingBuyer)
+
+        val shippingCost: EditText = findViewById(R.id.shippingCost)
+
         val calculateButton: Button = findViewById(R.id.calculate_button)
+
+        calculateButton.setOnClickListener{
+            val itemCostInt: Double = itemCost.text.toString().toDouble()
+            val sellPriceInt: Double = sellPrice.text.toString().toDouble()
+            val buyerShippingInt: Double = buyerShipping.text.toString().toDouble()
+            val shippingCostInt: Double = shippingCost.text.toString().toDouble()
+
+
+            val estimate = ((sellPriceInt + buyerShippingInt) - (itemCostInt + shippingCostInt) * (.15 + .30)).toString()
+            Toast.makeText(this@MainActivity, "The expected profit will be $estimate", Toast.LENGTH_LONG).show()
+
+
+
+
+        }
     }
+
+
     }
